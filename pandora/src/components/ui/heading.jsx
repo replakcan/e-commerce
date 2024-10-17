442ef81df8d195bd5ea06f1641789ad8baf11d59
Alper <alper.makcan@gmail.com>
2024-10-17 11:41:55 +0300

    Changes heading variant colors

--- pandora/src/components/ui/heading.jsx
+++ pandora/src/components/ui/heading.jsx
@@ -3,11 +3,11 @@ const Heading = ({ variant = "h1", children }) => {
 
     const headingStyles = {
         h1: "text-4xl font-bold text-gray-800",
-        h2: "text-3xl font-bold text-gray-700",
-        h3: "text-2xl font-bold text-gray-600",
-        h4: "text-xl font-bold text-gray-600",
-        h5: "text-lg font-semibold text-gray-500",
-        h6: "text-base font-semibold text-gray-500",
+        h2: "text-3xl font-bold text-gray-800",
+        h3: "text-2xl font-bold text-gray-800",
+        h4: "text-xl font-bold text-gray-800",
+        h5: "text-lg font-semibold text-gray-800",
+        h6: "text-base font-semibold text-gray-800",
         p: "text-base text-gray-500", // p varyantı için stil
     };
 
