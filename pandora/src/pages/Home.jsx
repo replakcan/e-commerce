dcc12a44efc241fd43a9e2ad7ebcaf3b7a674afb
Alper <alper.makcan@gmail.com>
2024-10-18 17:43:57 +0300

    Moves HomePage components to HomePage from App

--- pandora/src/pages/Home.jsx
+++ pandora/src/pages/Home.jsx
@@ -1,7 +1,24 @@
+import Clients from '../layouts/Clients'
+import Footer from '../layouts/Footer'
+import Header from '../layouts/Header'
+import ProductCard_1 from '../layouts/ProductCard_1'
+import ProductCard_2 from '../layouts/ProductCard_2'
+import ProductCard_3 from '../layouts/ProductCard_3'
+import ProductCard_4 from '../layouts/ProductCard_4'
+import ShopCard from '../layouts/ShopCard'
+
 const HomePage = () => {
 
-    return ( 
+    return (
         <>
+            <Header />
+            <ShopCard />
+            <ProductCard_1 />
+            <ProductCard_2 />
+            <ProductCard_3 />
+            <ProductCard_4 />
+            <Clients />
+            <Footer />
         </>
     )
 
