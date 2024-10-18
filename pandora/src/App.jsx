b57d4a2f2a8e7727813017e0b93751ebec0b2a8e
Alper <alper.makcan@gmail.com>
2024-10-18 17:29:41 +0300

    Adds ProductCard_4

--- pandora/src/App.jsx
+++ pandora/src/App.jsx
@@ -5,6 +5,7 @@ import Header from './layouts/Header'
 import ProductCard_1 from './layouts/ProductCard_1'
 import ProductCard_2 from './layouts/ProductCard_2'
 import ProductCard_3 from './layouts/ProductCard_3'
+import ProductCard_4 from './layouts/ProductCard_4'
 import ShopCard from './layouts/ShopCard'
 function App() {
 
@@ -16,6 +17,7 @@ function App() {
       <ProductCard_1 />
       <ProductCard_2 />
       <ProductCard_3 />
+      <ProductCard_4 />
       <Clients />
       <Footer />
     </main>
