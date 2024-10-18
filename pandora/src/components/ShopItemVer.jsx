c5695deb268c814483e89b32b19e467b1f0ddab3
Alper <alper.makcan@gmail.com>
2024-10-18 22:25:49 +0300

    Updates ShopItemVertical

--- pandora/src/components/ShopItemVer.jsx
+++ pandora/src/components/ShopItemVer.jsx
@@ -4,17 +4,16 @@ const ShopItemVer = () => {
 
     return (
 
-        <div className="flex flex-col items-center p-6 border border-gray-300 rounded-lg max-w-xs bg-white w-fit md:w-1/3">
-            <div className="mb-4">
-                <img src="https://placehold.co/600x800" alt="Caramel Cone Ice Cream" className="w-32 h-auto " />
+        <div className="flex flex-col items-center border p-1">
+            <img src="https://placehold.co/180x160" />
+            <div className="flex flex-col gap-2 p-2 items-center">
+                <Heading variant="h6">Graphic Design</Heading>
+                <p>English Department</p>
+                <p className="font-bold">$16.48 <span className="text-ikincil">$6.48</span></p>
             </div>
-            <Heading variant="h3">Graphic Design</Heading>
-            <p className="text-gray-500">English Department</p>
-            <div className="flex items-center space-x-2 mt-2">
-                <p className="text-gray-400 line-through text-sm">$16.48</p>
-                <p className="text-green-600 text-lg font-semibold">$6.48</p>
-            </div>
+
         </div>
+
 
     )
 }
