d3842134bf8c3e969ac53e1a12b7ee46b7269496
Alper <alper.makcan@gmail.com>
2024-10-18 18:49:19 +0300

    Moves ProductCard layouts to PageContent

--- pandora/src/pages/Home.jsx
+++ pandora/src/pages/Home.jsx
@@ -1,10 +1,7 @@
+import PageContent from '@/layouts/PageContent'
 import Clients from '../layouts/Clients'
 import Footer from '../layouts/Footer'
 import Header from '../layouts/Header'
-import ProductCard_1 from '../layouts/ProductCard_1'
-import ProductCard_2 from '../layouts/ProductCard_2'
-import ProductCard_3 from '../layouts/ProductCard_3'
-import ProductCard_4 from '../layouts/ProductCard_4'
 import ShopCard from '../layouts/ShopCard'
 
 const HomePage = () => {
@@ -13,10 +10,7 @@ const HomePage = () => {
         <>
             <Header />
             <ShopCard />
-            <ProductCard_1 />
-            <ProductCard_2 />
-            <ProductCard_3 />
-            <ProductCard_4 />
+            <PageContent />
             <Clients />
             <Footer />
         </>
