14607f36a17e880c239142dbd7f1b420adff1ee3
Alper <alper.makcan@gmail.com>
2024-10-18 19:17:29 +0300

    Creates BlogCard comp

---
+++ pandora/src/components/BlogCard.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Heading from './ui/heading';
+
+const BlogCard = () => {
+    return (
+        <div className='border w-fit flex flex-col gap-2 p-1'>
+            <img src="https://placehold.co/350x350" alt="" />
+            <div className='flex flex-col gap-3 p-8'>
+
+                <nav className='flex gap-3'>
+                    <a href="">Google</a>
+                    <a href="">Trending</a>
+                    <a href="">New</a>
+                </nav>
+                <Heading variant='h4'>Loudest à la Madison #1 (L'integral)</Heading>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+                <div className='flex justify-between'>
+                    <div className="flex gap-2">
+                        <i class="fa-solid fa-clock"></i>
+                        <p>22 April 2021</p>
+                    </div>
+                    <div className="flex gap-2">
+                        <i class="fa-solid fa-clock"></i>
+                        <p>10 comments</p>
+
+                    </div>
+                </div>
+                <a>Learn More</a>
+            </div>
+        </div>
+    )
+};
+
+export default BlogCard;
