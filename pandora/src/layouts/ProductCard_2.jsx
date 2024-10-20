d076fd59d8aa2d0d8cc934df0fa821c5d3975954
Alper <alper.makcan@gmail.com>
2024-10-20 15:54:58 +0300

    PopularCard_4 css

--- pandora/src/layouts/ProductCard_2.jsx
+++ pandora/src/layouts/ProductCard_2.jsx
@@ -9,11 +9,11 @@ const ProductCard_2 = () => {
     return (
 
         <section className="productCard_2 text-center flex flex-col items-center">
-            <img className="prc1" src="./src/assets/productCard/col-md-8.png" />
-            <div className="prc2 h-full flex flex-col justify-center gap-4 bg-footerTopBG px-8">
+            <img className="prc1 h-full w-full" src="./src/assets/productCard/col-md-8.png" />
+            <div className="prc2 h-full flex flex-col justify-center gap-4 bg-footerTopBG px-8 py-2">
                 <Heading variant="h3">MOST POPULAR</Heading>
                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur a, fuga in commodi excepturi et consequatur quas sequi tempore odio nobis labore architecto obcaecati iusto praesentium placeat repudiandae illo neque?</p>
-                <img className="py-5" src="https://placehold.co/600x400" />
+                <img className="py-5" src="https://placehold.co/300x300" />
                 <Heading variant="h4">English Department</Heading>
                 <p>$16.48<span className="text-ikincil">$16.48</span></p>
             </div>
--- pandora/src/layouts/ProductCard_4.jsx
+++ pandora/src/layouts/ProductCard_4.jsx
@@ -1,16 +1,20 @@
-import FeatureContent from "@/components/FeatureContent";
-import PopularItem from "@/components/MostPopularItem";
-
+import Heading from "@/components/ui/heading";
 
 const ProductCard_4 = () => {
 
 
     return (
 
-        <section className="productCard_4 flex flex-col items-center gap-4">
+        <section className="productCard_4 flex flex-col items-center">
             <img className="popularImg" src="./src/assets/productCard/col-md-9.png" />
-            <div className="popular">
-                <PopularItem />
+            <div className="popular w-full h-full">
+                <div className="h-full flex flex-col justify-center gap-4 bg-footerTopBG px-8 py-2">
+                    <Heading variant="h3">MOST POPULAR</Heading>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur a, fuga in commodi excepturi et consequatur quas sequi tempore odio nobis labore architecto obcaecati iusto praesentium placeat repudiandae illo neque?</p>
+                    <img className="py-5" src="https://placehold.co/300x300" />
+                    <Heading variant="h4">English Department</Heading>
+                    <p>$16.48<span className="text-ikincil">$16.48</span></p>
+                </div>
             </div>
         </section>
     )
