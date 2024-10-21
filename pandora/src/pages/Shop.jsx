c7eb83d0ed1baec2a7247c575414f42b64fd22a7
Alper <alper.makcan@gmail.com>
2024-10-21 13:33:12 +0300

    Adds HeaderNav @Shop

---
+++ pandora/src/pages/Shop.jsx
@@ -0,0 +1,16 @@
+import HeaderNav from "@/components/HeaderNav";
+
+const Shop = () => {
+
+
+    return (
+
+        <section className="Shop page">
+            <HeaderNav />
+
+
+        </section>
+
+    )
+}
+export default Shop;
