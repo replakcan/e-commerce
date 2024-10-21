bd2c393f71b117d8285830e9e669914e199e24bb
Alper <alper.makcan@gmail.com>
2024-10-21 14:50:52 +0300

    Creates SearchResults component

---
+++ pandora/src/components/SearchResults.jsx
@@ -0,0 +1,21 @@
+import Button from "./ui/button";
+
+const SearchResults = () => {
+    return (
+
+        <section className="flex flex-col items-center gap-2 md:flex-row md:justify-between">
+            <p> Showing all 12 results</p>
+            <div className="search-icon-btns flex flex-row items-center gap-3">
+                <p>Views:</p>
+                {/* //TODO BUTONLARA DOGRU ICON KOY */}
+                <Button variant="icon"><i class="fa-solid fa-arrow-left"></i></Button>
+                <Button variant="icon"><i class="fa-solid fa-arrow-left"></i></Button>
+            </div>
+            <div className="search-btns flex gap-3">
+                <Button variant="icon">Popularity</Button>
+                <Button>Filter</Button>
+            </div>
+        </section>
+    )
+}
+export default SearchResults;
