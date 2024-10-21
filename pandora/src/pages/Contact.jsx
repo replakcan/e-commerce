ed3215a1b3fb26b476aa33bf115ae84cb04a93e2
Alper <alper.makcan@gmail.com>
2024-10-21 21:31:58 +0300

    ContactPage final rev_00

--- pandora/src/pages/Contact.jsx
+++ pandora/src/pages/Contact.jsx
@@ -1,4 +1,5 @@
 import HeaderNav from "@/components/HeaderNav";
+import SupportCall from "@/components/SupportCall";
 import ContactInfo from "@/layouts/ContactInfo";
 import Footer from "@/layouts/Footer";
 import SupportInfo from "@/layouts/SupportInfo";
@@ -7,12 +8,13 @@ const Contact = () => {
 
     return (
 
-        <>
-        {/* <HeaderNav />
-        <ContactInfo /> */}
-        <SupportInfo />
-        {/* <Footer /> */}
-        </>
+        <div className="bg-blue-50 flex flex-col gap-4">
+            <HeaderNav />
+            <ContactInfo />
+            <SupportInfo />
+            <SupportCall />
+            <Footer />
+        </div>
     )
 }
 
