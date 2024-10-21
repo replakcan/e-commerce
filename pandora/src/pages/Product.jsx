5486ff0ee178201a409b1df0433056c012d9e49d
Alper <alper.makcan@gmail.com>
2024-10-21 18:31:36 +0300

    ProductPage rev00

--- pandora/src/pages/Product.jsx
+++ pandora/src/pages/Product.jsx
@@ -1,14 +1,25 @@
 import HeaderNav from "@/components/HeaderNav";
+import ProductDescription from "@/components/ProductDescription";
 import ProductDetail from "@/components/ProductDetail";
+import Clients from "@/layouts/Clients";
+import Footer from "@/layouts/Footer";
+import MoreProducts from "@/layouts/MoreProducts";
+
 
 const Product = () => {
 
     return (
 
-        <>
+        <div className="flex flex-col gap-8">
+
             <HeaderNav />
             <ProductDetail />
-        </>
+            <ProductDescription />
+            <MoreProducts />
+            <Clients />
+            <Footer/>
+        
+        </div>
     )
 }
 
