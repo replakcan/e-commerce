b66b46e397708c2c3ee5ac64c7ea1759299404bd
Alper <alper.makcan@gmail.com>
2024-10-21 15:06:50 +0300

    Updates ShopProductCards layout css

--- pandora/src/layouts/ShopProductCards.jsx
+++ pandora/src/layouts/ShopProductCards.jsx
@@ -1,9 +1,25 @@
+import ShopPageCard from "@/components/ShopPageCard";
+
 const ShopProductCards = () => {
 
     return (
 
-        <>
-        </>
+        <section className="flex flex-wrap justify-center md:justify-between ">
+
+            <ShopPageCard />
+            <ShopPageCard />
+            <ShopPageCard />
+            <ShopPageCard />
+            <ShopPageCard />
+            <ShopPageCard />
+            <ShopPageCard />
+            <ShopPageCard />
+            <ShopPageCard />
+            <ShopPageCard />
+            <ShopPageCard />
+            <ShopPageCard />
+
+        </section>
     )
 }
 
