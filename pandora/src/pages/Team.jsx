17e3c1b9c4b173e9ca0b559e4012838d15e00a16
Alper <alper.makcan@gmail.com>
2024-10-22 11:21:24 +0300

    TeamPage final rev00

--- pandora/src/pages/Team.jsx
+++ pandora/src/pages/Team.jsx
@@ -1,7 +1,18 @@
+import HeaderNav from "@/components/HeaderNav";
+import TeamGridHead from "@/components/TeamGridHead";
+import TeamMemberHead from "@/components/TeamMemberHead";
+import TeamGrid from "@/layouts/TeamGrid";
+import TeamMembers from "@/layouts/TeamMembers";
+
 const Team = () => {
 
     return (
         <>
+        <HeaderNav />
+        <TeamGridHead />
+        <TeamGrid />
+        <TeamMemberHead />
+        <TeamMembers />
         </>
     )
 }
