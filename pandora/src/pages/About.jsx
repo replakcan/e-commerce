7316c3aadf4fcee32e81a79d6b288dcf0eb43039
Alper <alper.makcan@gmail.com>
2024-10-22 14:46:45 +0300

    AboutPage final rev_00

--- pandora/src/pages/About.jsx
+++ pandora/src/pages/About.jsx
@@ -1,16 +1,24 @@
 import HeaderNav from "@/components/HeaderNav";
 import TeamMemberHead from "@/components/TeamMemberHead";
+import AboutContent from "@/layouts/AboutContent";
 import AboutInfo from "@/layouts/AboutInfo";
+import AboutOffer from "@/layouts/AboutOffer";
+import Clients from "@/layouts/Clients";
+import Footer from "@/layouts/Footer";
 import TeamMembers from "@/layouts/TeamMembers";
 
 const About = () => {
 
     return (
         <>
-        <HeaderNav />
-        <AboutInfo />
-        <TeamMemberHead />
-        <TeamMembers />
+            <HeaderNav />
+            <AboutInfo />
+            <AboutContent />
+            <TeamMemberHead />
+            <TeamMembers />
+            <Clients />
+            <AboutOffer />
+            <Footer />
         </>
     )
 }
