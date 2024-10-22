be6d2ae3ab36c5e512e75a63bf66dc963d1b23a9
Alper <alper.makcan@gmail.com>
2024-10-22 10:59:37 +0300

    Creates TeamGridHead component

---
+++ pandora/src/components/TeamGridHead.jsx
@@ -0,0 +1,14 @@
+import { Divide } from "lucide-react";
+import Heading from "./ui/heading";
+
+const TeamGridHead = () => {
+
+    return (
+        <div className="flex flex-col gap-3 items-center">
+            <p>WHAT WE DO</p>
+            <Heading variant="h2">Innovation tailored for you</Heading>
+            <p><span className="font-bold">Home</span>Team</p>
+        </div>
+    )
+}
+export default TeamGridHead;
