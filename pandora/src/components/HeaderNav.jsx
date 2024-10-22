39a30074f3770d2ad4c34b24ab759778cd1396ce
Alper <alper.makcan@gmail.com>
2024-10-22 21:19:00 +0300

    Update headerNav paddings

--- pandora/src/components/HeaderNav.jsx
+++ pandora/src/components/HeaderNav.jsx
@@ -9,17 +9,17 @@ const HeaderNav = () => {
     let history = useHistory();
 
     return (
-        <div className="flex flex-col items-center md:flex-row bg-ikincil p-6">
+        <div className="flex flex-col items-center md:flex-row bg-ikincil md:py-3 md:px-[10%]">
             <div className="header-nav flex justify-between items-center gap-4 w-full">
                 <Heading variant="h3">Pandora</Heading>
-                <ul className="flex gap-4">
+                <ul className="flex gap-4 md:pr-4">
                     <i className="fa-regular fa-user"></i>
                     <i className="fa-solid fa-magnifying-glass"></i>
                     <i className="fa-solid fa-cart-shopping"></i>
                     <i className="fa-solid fa-bars"></i>
                 </ul>
             </div>
-            <div className="flex flex-col gap-2 md:flex-row py-12">
+            <div className="flex flex-col gap-2 md:flex-row py-12 md:py-3">
                 <Button onClick={() => history.push("/")} variant="ghost" className="md:text-birincil">Home</Button>
                 <Button onClick={() => history.push("/shop")} variant="ghost" className="md:text-birincil">Shop</Button>
                 <Button onClick={() => history.push("/product")} variant="ghost" className="md:text-birincil">Product</Button>
