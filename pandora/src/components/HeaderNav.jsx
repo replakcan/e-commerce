1ba4993631d163bc0077c3decbb69616320e5cdd
Alper <alper.makcan@gmail.com>
2024-10-24 01:53:19 +0300

    Updates headernav colors

--- pandora/src/components/HeaderNav.jsx
+++ pandora/src/components/HeaderNav.jsx
@@ -9,7 +9,7 @@ const HeaderNav = () => {
     let history = useHistory();
 
     return (
-        <div className="flex flex-col items-center md:flex-row bg-ikincil md:px-[10%]">
+        <div className="flex flex-col items-center md:flex-row bg-white md:px-[10%]">
             <div className="header-nav flex justify-between items-center gap-4 w-full">
                 <Heading variant="h3">Pandora</Heading>
                 <ul className="flex gap-4 md:pr-4">
@@ -20,13 +20,13 @@ const HeaderNav = () => {
                 </ul>
             </div>
             <div className="flex flex-col  md:flex-row py-12 md:py-3">
-                <Button onClick={() => history.push("/signup")} variant="ghost" size="sm" className="md:text-birincil">SignUp</Button>
-                <Button onClick={() => history.push("/")} variant="ghost" size="sm" className="md:text-birincil">Home</Button>
-                <Button onClick={() => history.push("/shop")} variant="ghost" size="sm" className="md:text-birincil">Shop</Button>
-                <Button onClick={() => history.push("/product")} variant="ghost" size="sm" className="md:text-birincil">Product</Button>
-                <Button onClick={() => history.push("/about")} variant="ghost" size="sm" className="md:text-birincil">About</Button>
-                <Button onClick={() => history.push("/contact")} variant="ghost" size="sm" className="md:text-birincil">Contact</Button>
-                <Button onClick={() => history.push("/team")} variant="ghost" size="sm" className="md:text-birincil">Team</Button>
+                <Button onClick={() => history.push("/signup")} variant="ghost" size="sm" className="md:text-ikincil">SignUp</Button>
+                <Button onClick={() => history.push("/")} variant="ghost" size="sm" className="md:text-ikincil">Home</Button>
+                <Button onClick={() => history.push("/shop")} variant="ghost" size="sm" className="md:text-ikincil">Shop</Button>
+                <Button onClick={() => history.push("/product")} variant="ghost" size="sm" className="md:text-ikincil">Product</Button>
+                <Button onClick={() => history.push("/about")} variant="ghost" size="sm" className="md:text-ikincil">About</Button>
+                <Button onClick={() => history.push("/contact")} variant="ghost" size="sm" className="md:text-ikincil">Contact</Button>
+                <Button onClick={() => history.push("/team")} variant="ghost" size="sm" className="md:text-ikincil">Team</Button>
             </div>
         </div>
 
