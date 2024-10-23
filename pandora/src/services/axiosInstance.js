a356a42a9e4b4c78b1145850a85b5bcfa901917f
Alper <alper.makcan@gmail.com>
2024-10-23 17:30:13 +0300

    Creates axiosInstance under services folder

---
+++ pandora/src/services/axiosInstance.js
@@ -0,0 +1,12 @@
+// src/services/axiosInstance.js
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: "https://workintech-fe-ecommerce.onrender.com", // Base URL
+  timeout: 1000, // 10 saniye timeout
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default axiosInstance;
