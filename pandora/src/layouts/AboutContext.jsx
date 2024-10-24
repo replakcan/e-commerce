bcd020594f280cbc69fc26d3df79ee8506db7d0a
Alper <alper.makcan@gmail.com>
2024-10-24 19:57:21 +0300

    Fixes responsive problems @AboutPage

--- pandora/src/layouts/AboutContext.jsx
+++ pandora/src/layouts/AboutContext.jsx
@@ -4,31 +4,33 @@ import Heading from "@/components/ui/heading";
 const AboutContext = () => {
 
     return (
-        <div>
-            <div className="flex flex-col items-center text-center">
-                <p className="text-danger">Problems trying</p>
-                <p className="font-bold">Met minim Mollie non desert Alamo est sit cliquey dolor do met sent.</p>
-                <p>Problems trying to resolve the conflict between the two major realms of Classical physics: Newtonian mechanics </p>
+        <div className="flex flex-col gap-24">
+            <div className="flex flex-col items-center text-center md:flex-row gap-8 ">
+                <div className="flex flex-col gap-3 md:text-left">
+                    <p className="text-danger">Problems trying</p>
+                    <Heading variant="h6" className="font-bold">Met minim Mollie non desert Alamo est sit cliquey dolor do met sent.</Heading>
+                </div>
+                <p className="text-left">Problems trying to resolve the conflict between the two major realms of Classical physics: Newtonian mechanics </p>
             </div>
-            <div className="flex flex-col text-center gap-10">
+            <div className="flex flex-col text-center gap-10 md:flex-row md:justify-between">
                 <div>
                     <Heading variant="h1">15K</Heading>
-                    <Heading variant="h5">Happy Customer</Heading>
+                    <Heading variant="h6">Happy Customer</Heading>
                 </div>
                 <div>
                     <Heading variant="h1">150K</Heading>
-                    <Heading variant="h5">Happy Customer</Heading>
+                    <Heading variant="h6">Happy Customer</Heading>
                 </div>
                 <div>
                     <Heading variant="h1">15</Heading>
-                    <Heading variant="h5">Happy Customer</Heading>
+                    <Heading variant="h6">Happy Customer</Heading>
                 </div>
                 <div>
                     <Heading variant="h1">100+</Heading>
-                    <Heading variant="h5">Happy Customer</Heading>
+                    <Heading variant="h6">Happy Customer</Heading>
                 </div>
             </div>
-                {/* <VideoComponent /> */}
+            {/* <VideoComponent /> */}
             <div>
 
             </div>
--- pandora/src/layouts/AboutInfo.jsx
+++ pandora/src/layouts/AboutInfo.jsx
@@ -4,11 +4,11 @@ import Heading from "@/components/ui/heading";
 const AboutInfo = () => {
 
     return (
-        <div className="flex flex-col gap-3 md:flex-row md:justify-between ">
+        <div className="flex flex-col gap-3 md:flex-row md:justify-between py-12">
             <div className="flex flex-col items-center md:w-1/2 md:items-start md:justify-center gap-4">
                 <p className="font-bold hidden md:block">ABOUT COMPANY</p>
                 <Heading variant="h2">ABOUT US</Heading>
-                <p className="md:w-2/3">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque voluptates rerum explicabo beatae ipsam omnis aspernatur sequi repudiandae inventore deserunt sint, tempora, earum labore doloremque eum quae non sed quibusdam.</p>
+                <p className="md:w-2/3">Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
                 <Button>Get Quote Now</Button>
             </div>
             <div className=""> 
