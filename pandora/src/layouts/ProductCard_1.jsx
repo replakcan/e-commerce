920fb327bc2a973fb79aebedad80788a91551882
Alper <alper.makcan@gmail.com>
2024-10-25 13:11:16 +0300

    Updates product card nav css

--- pandora/src/layouts/ProductCard_1.jsx
+++ pandora/src/layouts/ProductCard_1.jsx
@@ -10,12 +10,14 @@ const ProductCard_1 = () => {
 
         <div className="productCard flex flex-col md:grid md:grid-cols-3 md:grid-rows-[min-content_1fr]">
             <img className=" md:col-start-1 col-end-2 md:row-span-full bg-center bg-no-repeat w-full h-full" src="./images/productCard/card-cover-5.png" />
-            <nav className="productNav flex flex-col items-center md:col-start-2 col-end-4 md:row-span-1 md:flex-row md:justify-between md:px-7 md:py-6 border-b-1 border-blue-500 gap-6 pt-24">
-                <Heading variant="h3">BESTSELLER PRODUCTS</Heading>
-                <div className="flex gap-8">
-                    <a href="">Men</a>
-                    <a href="">Women</a>
-                    <a href="">Accessories</a>
+            <nav className="productNav flex flex-col items-center md:col-start-2 col-end-4 md:row-span-1 md:flex-row md:justify-between md:px-7 md:py-6 border-b-1 border-blue-500 gap-6 pt-24 flex-wrap pb-12">
+                <div className="flex flex-col md:flex-row items-center justify-between grow">
+                    <Heading variant="h3">BESTSELLER PRODUCTS</Heading>
+                    <div className="flex gap-8">
+                        <a href="">Men</a>
+                        <a href="">Women</a>
+                        <a href="">Accessories</a>
+                    </div>
                 </div>
                 <div className="flex gap-3">
                     <Button variant="outline" size="icon"><ChevronLeft /></Button>
--- pandora/src/layouts/ProductCard_3.jsx
+++ pandora/src/layouts/ProductCard_3.jsx
@@ -10,14 +10,16 @@ const ProductCard_3 = () => {
 
         <div className="productCard flex flex-col md:grid md:grid-cols-3 md:grid-rows-[min-content_1fr]">
             <img className=" md:col-start-3 md:col-end-4 md:row-start-1 md:row-end-3 md:w-full h-full" src="./images/productCard/col-md-4.png" />
-            <nav className="productNav flex flex-col items-center md:col-span-2 md:row-span-1 md:flex-row md:justify-between md:px-7 md:py-6">
-                <Heading variant="h3">BESTSELLER PRODUCTS</Heading>
-                <div className="flex gap-8">
-                    <a href="">Men</a>
-                    <a href="">Women</a>
-                    <a href="">Accessories</a>
+            <nav className="productNav flex flex-col items-center md:col-span-2 md:row-span-1 md:flex-row md:justify-between md:px-7 md:py-6 gap-6 flex-wrap">
+                <div className="flex flex-col md:flex-row items-center justify-between grow">
+                    <Heading variant="h3">BESTSELLER PRODUCTS</Heading>
+                    <div className="flex gap-8">
+                        <a href="">Men</a>
+                        <a href="">Women</a>
+                        <a href="">Accessories</a>
+                    </div>
                 </div>
-                <div>
+                <div className="flex gap-3">
                     <Button variant="outline" size="icon"><ChevronLeft /></Button>
                     <Button variant="outline" size="icon"><ChevronRight /></Button>
                 </div>
