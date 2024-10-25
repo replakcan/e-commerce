b96f81e4ec95f8e85046ff2f6ea30cb576cc4d4c
Alper <alper.makcan@gmail.com>
2024-10-25 13:15:18 +0300

    Updates product card mobile paddings

--- pandora/src/layouts/ProductCard_3.jsx
+++ pandora/src/layouts/ProductCard_3.jsx
@@ -10,7 +10,7 @@ const ProductCard_3 = () => {
 
         <div className="productCard flex flex-col md:grid md:grid-cols-3 md:grid-rows-[min-content_1fr]">
             <img className=" md:col-start-3 md:col-end-4 md:row-start-1 md:row-end-3 md:w-full h-full" src="./images/productCard/col-md-4.png" />
-            <nav className="productNav flex flex-col items-center md:col-span-2 md:row-span-1 md:flex-row md:justify-between md:px-7 md:py-6 gap-6 flex-wrap">
+            <nav className="productNav flex flex-col items-center md:col-span-2 md:row-span-1 md:flex-row md:justify-between md:px-7 md:py-6 gap-6 flex-wrap pt-24 pb-12">
                 <div className="flex flex-col md:flex-row items-center justify-between grow">
                     <Heading variant="h3">BESTSELLER PRODUCTS</Heading>
                     <div className="flex gap-8">
@@ -24,7 +24,7 @@ const ProductCard_3 = () => {
                     <Button variant="outline" size="icon"><ChevronRight /></Button>
                 </div>
             </nav>
-            <div className="productContent flex flex-wrap md:col-start-1 md:col-end-3 md:flex-row md:justify-between row-start-2 row-end-3 justify-center md:pr-7 gap-7 md:gap-0">
+            <div className="productContent flex flex-wrap md:col-start-1 md:col-end-3 md:flex-row md:justify-between row-start-2 row-end-3 justify-center md:pr-7 gap-7 md:gap-0 pt-2">
                 <ShopItemVer />
                 <ShopItemVer />
                 <ShopItemVer />
--- pandora/src/layouts/ProductCard_5.jsx
+++ pandora/src/layouts/ProductCard_5.jsx
@@ -10,7 +10,7 @@ const ProductCard_5 = () => {
             <div className="py-12 md:text-left">
                 <Heading variant="h3">BESTSELLER PRODUCTS</Heading>
             </div>
-            <div className="flex flex-wrap items-center justify-center md:flex-row gap-7 md:gap-0">
+            <div className="flex flex-wrap items-center justify-center md:flex-row gap-7 md:gap-0 pb-12">
                 <BestSellerCard />
                 <BestSellerCard />
                 <BestSellerCard />
