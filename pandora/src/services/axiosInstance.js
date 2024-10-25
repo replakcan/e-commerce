9c76dd8c35ba221caa5474b27034135fe1776945
Alper <alper.makcan@gmail.com>
2024-10-25 18:02:07 +0300

    Deletes comments

--- pandora/src/services/axiosInstance.js
+++ pandora/src/services/axiosInstance.js
@@ -1,9 +1,8 @@
-// src/services/axiosInstance.js
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://workintech-fe-ecommerce.onrender.com", // Base URL
-  timeout: 1000, // 1 saniye timeout
+  baseURL: "https://workintech-fe-ecommerce.onrender.com",
+  timeout: 1000,
   headers: {
     "Content-Type": "application/json",
   },
