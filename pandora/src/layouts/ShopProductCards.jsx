7543ba50bde787ec30c33d46c7f8d069f4f656ee
Alper <alper.makcan@gmail.com>
2024-10-27 18:30:13 +0300

    Maps products

--- pandora/src/layouts/ShopProductCards.jsx
+++ pandora/src/layouts/ShopProductCards.jsx
@@ -1,23 +1,22 @@
 import ShopPageCard from "@/components/ShopPageCard";
+import { useSelector } from "react-redux";
 
 const ShopProductCards = () => {
 
+    const products = useSelector((store) => store.product.productList);
+    console.log("PRODUCTS: ", products)
     return (
 
         <div className="flex flex-wrap justify-center md:justify-between gap-3">
 
-            <ShopPageCard />
-            <ShopPageCard />
-            <ShopPageCard />
-            <ShopPageCard />
-            <ShopPageCard />
-            <ShopPageCard />
-            <ShopPageCard />
-            <ShopPageCard />
-            <ShopPageCard />
-            <ShopPageCard />
-            <ShopPageCard />
-            <ShopPageCard />
+            {products.products.map((product) => {
+                return <ShopPageCard
+                    key={product.id}
+                    name={product.name}
+                    description={product.description}
+                    price={product.price}
+                    src={product.images[0].url} />
+            })}
 
         </div>
     )
