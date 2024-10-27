0584382079dca6d5fe288485377cf68414acc606
Alper <alper.makcan@gmail.com>
2024-10-27 17:56:17 +0300

    Updates CategoryCard w/response.data

--- pandora/src/components/CategoryCard.jsx
+++ pandora/src/components/CategoryCard.jsx
@@ -1,18 +1,20 @@
+import { Link } from "react-router-dom";
 import Heading from "./ui/heading";
 
-const CategoryCard = () => {
+const CategoryCard = ({ title, src, gender, category }) => {
 
 
     return (
-
-        <div className="grid grid-cols-3 grid-rows-3 w-fit">
-            <img className="col-start-1 col-end-4 row-start-1 row-end-4 " src="https://placehold.co/250x250">
-            </img>
-            <div className="col-start-2 row-start-2 col-end-3 row-end-3 flex flex-col justify-around items-center">
-                <Heading variant="h6">CLOTHS</Heading>
-                <p>5 Items</p>
+        <Link to={`/shop/${gender}/${category}`}>
+            <div className="grid grid-cols-3 grid-rows-3 max-h-[250px] max-w-[250px] overflow-hidden">
+                <img className="col-start-1 col-end-4 row-start-1 row-end-4 object-cover" src={src}>
+                </img>
+                <div className="col-start-2 row-start-2 col-end-3 row-end-3 flex flex-col justify-around items-center">
+                    <Heading variant="h6">{title}</Heading>
+                    <p>5 Items</p>
+                </div>
             </div>
-        </div>
+        </Link>
     )
 }
 
