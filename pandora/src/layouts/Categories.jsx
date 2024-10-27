3d4c1ae17bcbc9f6532736905845a7659089bb12
Alper <alper.makcan@gmail.com>
2024-10-27 17:55:46 +0300

    Shows top 5 categories by rating

--- pandora/src/layouts/Categories.jsx
+++ pandora/src/layouts/Categories.jsx
@@ -1,21 +1,23 @@
 import CategoryCard from "@/components/CategoryCard";
 import CategoryTop from "@/components/CategoryTop";
+import { useSelector } from "react-redux";
 
 const Categories = () => {
-
+    const categories = useSelector((store) => store.product.categories);
+    
+    const topCategories = categories.sort((a, b) => b.rating - a.rating).slice(0, 5);
+    console.log("topCategories: ", topCategories);
 
     return (
         <>
             <div className="bg-light_gray md:px-[10%] py-6">
                 <CategoryTop />
             </div>
-            <div className="flex flex-col items-center gap-0 md:flex-row flex-wrap justify-between bg-light_gray md:px-[10%] md:pb-12">
-
-                <CategoryCard />
-                <CategoryCard />
-                <CategoryCard />
-                <CategoryCard />
-                <CategoryCard />
+            <div className="flex flex-col items-center gap-0 md:flex-row flex-wrap justify-between bg-light_gray md:px-[10%] md:pb-12 row-gap-4 ">
+            
+                {topCategories.map((topcat) => {
+                    return <CategoryCard gender={topcat.gender} category={topcat.code} key={topcat.id} title={topcat.title} src={topcat.img} />
+                })}
 
             </div>
         </>
