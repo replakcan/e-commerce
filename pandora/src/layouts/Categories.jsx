df53518f0b36905f4ea0a407bc2e1cee1f4ded1c
Alper <alper.makcan@gmail.com>
2024-10-27 18:30:33 +0300

    Formats Categories component

--- pandora/src/layouts/Categories.jsx
+++ pandora/src/layouts/Categories.jsx
@@ -4,8 +4,8 @@ import { useSelector } from "react-redux";
 
 const Categories = () => {
     const categories = useSelector((store) => store.product.categories);
-    
-    const topCategories = categories.sort((a, b) => b.rating - a.rating).slice(0, 5);
+
+    const topCategories = [...categories].sort((a, b) => b.rating - a.rating).slice(0, 5);
     console.log("topCategories: ", topCategories);
 
     return (
@@ -14,9 +14,14 @@ const Categories = () => {
                 <CategoryTop />
             </div>
             <div className="flex flex-col items-center gap-0 md:flex-row flex-wrap justify-between bg-light_gray md:px-[10%] md:pb-12 row-gap-4 ">
-            
+
                 {topCategories.map((topcat) => {
-                    return <CategoryCard gender={topcat.gender} category={topcat.code} key={topcat.id} title={topcat.title} src={topcat.img} />
+                    return <CategoryCard
+                        gender={topcat.gender}
+                        category={topcat.code}
+                        key={topcat.id}
+                        title={topcat.title}
+                        src={topcat.img} />
                 })}
 
             </div>
