f6cdddf75c74c872967a7cd4378309fdbb6b1ecb
Alper <alper.makcan@gmail.com>
2024-10-28 21:27:26 +0300

    Adds new thunk-action for sort param

--- pandora/src/redux/actions/productActions.js
+++ pandora/src/redux/actions/productActions.js
@@ -79,7 +79,7 @@ export const fetchProducts = () => {
 export const fetchProductsByCategory = (categoryId) => {
     return async (dispatch, getState) => {
         dispatch({ type: SET_FETCH_STATE, payload: "FETCHING" });
-        
+
         await axiosInstance(`/products?category=${categoryId}`)
             .then((response) => {
                 dispatch({
@@ -94,3 +94,46 @@ export const fetchProductsByCategory = (categoryId) => {
             });
     };
 };
+
+export const fetchProductsBySortParam = (sort) => {
+    return async (dispatch, getState) => {
+        dispatch({ type: SET_FETCH_STATE, payload: "FETCHING" });
+
+        await axiosInstance(`/products?sort=${sort}`)
+            .then((response) => {
+                dispatch({
+                    type: SET_PRODUCT_LIST,
+                    payload: response.data.products,
+                });
+                dispatch({ type: SET_TOTAL, payload: response.data.total });
+                dispatch({ type: SET_FETCH_STATE, payload: "FETCHED" });
+            })
+            .catch((error) => {
+                dispatch({ type: SET_FETCH_STATE, payload: "FAILED" });
+            });
+    };
+};
+
+/* export const fetchProductsByCategory = (categoryId, sort) => {
+    return async (dispatch, getState) => {
+        dispatch({ type: SET_FETCH_STATE, payload: "FETCHING" });
+        
+        let url = `/products?category=${categoryId}`;
+        if (sort) {
+            url += `&sort=${sort}`;
+        }
+
+        await axiosInstance(url)
+            .then((response) => {
+                dispatch({
+                    type: SET_PRODUCT_LIST,
+                    payload: response.data.products,
+                });
+                dispatch({ type: SET_TOTAL, payload: response.data.total });
+                dispatch({ type: SET_FETCH_STATE, payload: "FETCHED" });
+            })
+            .catch((error) => {
+                dispatch({ type: SET_FETCH_STATE, payload: "FAILED" });
+            });
+    };
+}; */
