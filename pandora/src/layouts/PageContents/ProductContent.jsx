e187cd1527a2a537b3ef8491add8ad4f7d072b74
Alper <alper.makcan@gmail.com>
2024-10-30 15:19:38 +0300

    Adds Back btn @ProductDetailPage

--- pandora/src/layouts/PageContents/ProductContent.jsx
+++ pandora/src/layouts/PageContents/ProductContent.jsx
@@ -2,12 +2,32 @@ import ProductDescription from "@/components/ProductDescription";
 import Clients from "../Clients";
 import MoreProducts from "../MoreProducts";
 import ProductDetail from "@/components/ProductDetail";
+import { useSelector } from "react-redux";
+import { Button } from "@/components/ui/button";
+import { useHistory } from "react-router-dom";
 
 const ProductContent = () => {
 
+    const productInfo = useSelector((store) => store.product.product)
+    const history = useHistory();
+    
+    const handleBackClick = () => {
+        history.goBack();
+    };
+
     return (
         <div className="content py-12">
-            <ProductDetail />
+            <Button
+                onClick={handleBackClick}
+            >Back</Button>
+            <ProductDetail
+                src={productInfo.images[0].url}
+                name={productInfo.name}
+                description={productInfo.description}
+                stock={productInfo.stock}
+                price={productInfo.price}
+                sell_count={productInfo.sell_count}
+            />
             <section>
                 <ProductDescription />
             </section>
