f6028abb13d89250954d40876dcd3307aa11481b
Alper <alper.makcan@gmail.com>
2024-10-31 12:35:19 +0300

    Creates orderPage specific actions
    increment-decrement-toggleCheck-remove actions

--- pandora/src/redux/actions/shoppingCartActions.js
+++ pandora/src/redux/actions/shoppingCartActions.js
@@ -1,6 +1,10 @@
 export const SET_CART = 'SET_CART';
 export const SET_PAYMENT = 'SET_PAYMENT';
 export const SET_ADDRESS = 'SET_ADDRESS';
+export const INCREMENT_COUNT = "INCREMENT_COUNT";
+export const DECREMENT_COUNT = "DECREMENT_COUNT";
+export const REMOVE_FROM_CART = "REMOVE_FROM_CART";
+export const TOGGLE_CHECKED = "TOGGLE_CHECKED";
 
 
 export const setCart = (product) => ({
@@ -16,4 +20,24 @@ export const setPayment = (payment) => ({
 export const setAddress = (address) => ({
     type: SET_ADDRESS,
     payload: address,
+});
+
+export const incrementCount = (productId) => ({
+    type: INCREMENT_COUNT,
+    payload: productId,
+});
+
+export const decrementCount = (productId) => ({
+    type: DECREMENT_COUNT,
+    payload: productId,
+});
+
+export const removeFromCart = (productId) => ({
+    type: REMOVE_FROM_CART,
+    payload: productId,
+});
+
+export const toggleChecked = (productId) => ({
+    type: TOGGLE_CHECKED,
+    payload: productId,
 });
--- pandora/src/redux/reducers/shoppingCartReducer.js
+++ pandora/src/redux/reducers/shoppingCartReducer.js
@@ -1,4 +1,4 @@
-import { SET_ADDRESS, SET_CART, SET_PAYMENT } from "../actions/shoppingCartActions";
+import { DECREMENT_COUNT, INCREMENT_COUNT, REMOVE_FROM_CART, SET_ADDRESS, SET_CART, SET_PAYMENT, TOGGLE_CHECKED } from "../actions/shoppingCartActions";
 
 const initialState = {
     cart: [],
@@ -30,6 +30,61 @@ export default function ShoppingCartReducer(state = initialState, action) {
                     { count: 1, checked: true, product: action.payload },
                 ],
             };
+        case INCREMENT_COUNT: {
+            const existingProductIndex = state.cart.findIndex(
+                (item) => item.product.id === action.payload
+            );
+
+            if (existingProductIndex >= 0) {
+                const updatedCart = [...state.cart];
+                updatedCart[existingProductIndex] = {
+                    ...updatedCart[existingProductIndex],
+                    count: updatedCart[existingProductIndex].count + 1,
+                };
+                return { ...state, cart: updatedCart };
+            }
+            return state; // Ürün yoksa state'i değiştirme
+        }
+
+        case DECREMENT_COUNT: {
+            const existingProductIndex = state.cart.findIndex(
+                (item) => item.product.id === action.payload
+            );
+
+            if (existingProductIndex >= 0) {
+                const updatedCart = [...state.cart];
+                const newCount = updatedCart[existingProductIndex].count - 1;
+
+                if (newCount > 0) {
+                    updatedCart[existingProductIndex] = {
+                        ...updatedCart[existingProductIndex],
+                        count: newCount,
+                    };
+                } else {
+                    updatedCart.splice(existingProductIndex, 1); // Count 0'a ulaşırsa ürünü kaldır
+                }
+
+                return { ...state, cart: updatedCart };
+            }
+            return state; // Ürün yoksa state'i değiştirme
+        }
+
+        case REMOVE_FROM_CART: {
+            return {
+                ...state,
+                cart: state.cart.filter((item) => item.product.id !== action.payload),
+            };
+        }
+
+        case TOGGLE_CHECKED: {
+            const updatedCart = state.cart.map((item) =>
+                item.product.id === action.payload
+                    ? { ...item, checked: !item.checked }
+                    : item
+            );
+            return { ...state, cart: updatedCart };
+        }
+        
         case SET_PAYMENT:
             return {
                 ...state,
