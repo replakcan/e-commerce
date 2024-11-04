fc294d3e31dc7d7d0630dca97769c6c96383416f
Alper <alper.makcan@gmail.com>
2024-11-04 12:55:45 +0300

    Updates saved-address-infos when an address is deleted

--- pandora/src/components/AddressInfo.jsx
+++ pandora/src/components/AddressInfo.jsx
@@ -1,18 +1,19 @@
 import { useDispatch, useSelector } from "react-redux";
 import Heading from "./ui/heading";
 import { Trash } from "lucide-react";
-import { deleteAddress } from "@/redux/actions/clientActions";
+import { deleteAddress, fetchAddressList } from "@/redux/actions/clientActions";
 import { useState } from "react";
 import { Button } from "./ui/button";
 import AddressUpdateForm from "@/layouts/AddressUpdateForm";
 
-const AddressInfo = ({ title, name, surname, phone, city, district, neighborhood, address, id }) => {
+const AddressInfo = ({ title, name, surname, phone, city, district, neighborhood, address, id, onClick }) => {
     const dispatch = useDispatch();
     const userToken = useSelector((store) => store.client.user.token);
     const [isEditing, setIsEditing] = useState(false);
 
     const deleteAddressInfo = () => {
         dispatch(deleteAddress(id, userToken));
+        dispatch(fetchAddressList(userToken))
     };
 
     const handleEditClick = () => {
@@ -24,7 +25,7 @@ const AddressInfo = ({ title, name, surname, phone, city, district, neighborhood
     };
 
     return (
-        <div className="border border-black w-[33%] flex justify-between p-2">
+        <div className="border border-black flex justify-between p-2 min-w-[300px]">
             {isEditing ? (
                 <AddressUpdateForm
                     id={id}
@@ -32,14 +33,14 @@ const AddressInfo = ({ title, name, surname, phone, city, district, neighborhood
                     onClose={handleCloseEditForm}
                 />
             ) : (
-                <div>
+                <div className="flex flex-col flex-wrap" onClick={onClick}>
                     <Heading variant="h2">{title}</Heading>
                     <Heading variant="h4">{name} {surname}</Heading>
                     <p><span className="text-danger font-bold">phone:</span> {phone}</p>
                     <p><span className="text-danger font-bold">city:</span> {city}</p>
                     <p><span className="text-danger font-bold">district:</span> {district}</p>
                     <p><span className="text-danger font-bold">neighborhood:</span> {neighborhood}</p>
-                    <p><span className="text-danger font-bold">address:</span> {address}</p>
+                    <p className="max-w-[200px]"><span className="text-danger font-bold">address:</span> {address}</p>
                 </div>
             )}
             <div className="flex flex-col justify-between items-end">
