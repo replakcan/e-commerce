2260fefc9f0423db8e4c96899e61e50282a2c619
Alper <alper.makcan@gmail.com>
2024-11-05 12:37:19 +0300

    Adds padding to footerItems

--- pandora/src/components/FeatureContent.jsx
+++ pandora/src/components/FeatureContent.jsx
@@ -3,7 +3,7 @@ import FeatureCard from "./FeatureCard";
 const FeatureContent = () => {
 
     return (
-        <div className="feature-content flex items-start flex-col gap-4 justify-between md:flex-row p-6 bg-light_gray">
+        <div className="feature-content flex items-start flex-col gap-4 justify-between md:flex-row md:p-6 bg-light_gray py-12">
             <FeatureCard />
             <FeatureCard />
             <FeatureCard />
