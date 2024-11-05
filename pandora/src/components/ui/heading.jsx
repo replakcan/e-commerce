e06331e2443479d3aa4208754318f157c8360b45
Alper <alper.makcan@gmail.com>
2024-11-05 20:09:13 +0300

    Deletes unneccessary comments

--- pandora/src/components/ui/heading.jsx
+++ pandora/src/components/ui/heading.jsx
@@ -3,12 +3,12 @@ const Heading = ({ variant = "h1", color = "text-gray-800", className = "", chil
 
     const headingStyles = {
         h1: `text-7xl font-bold ${color}`,
-        h2: `text-3xl font-bold ${color}`,
+        h2: `text-5xl font-bold ${color}`,
         h3: `text-2xl font-bold ${color}`,
         h4: `text-xl font-bold ${color}`,
         h5: `text-lg font-semibold ${color}`,
         h6: `text-base font-semibold ${color}`,
-        p: `text-base ${color === "text-gray-800" ? "text-gray-500" : color}`, // default gray for <p>
+        p: `text-base ${color === "text-gray-800" ? "text-gray-500" : color}`,
     };
 
     return <Tag className={`${headingStyles[variant]} ${className}`}>{children}</Tag>;
