21c9efd4640808ec2645b7083bbfd01104611767
Alper <alper.makcan@gmail.com>
2024-11-06 17:42:23 +0300

    Connects topCategory links @shop

--- pandora/src/components/CategoryCard.jsx
+++ pandora/src/components/CategoryCard.jsx
@@ -1,17 +1,16 @@
 import { Link } from "react-router-dom";
 import Heading from "./ui/heading";
 
-const CategoryCard = ({ title, src, gender, category }) => {
-
+const CategoryCard = ({ title, src, gender, category, id }) => {
 
     return (
-        <Link className="flex justify-center" to={`/shop/${gender}/${category}`}>
+        <Link className="flex justify-center" to={`/shop/${gender}/${category}/${id}`}>
             <div className="grid w-[80%] grid-cols-3 grid-rows-3 md:max-h-[250px] md:max-w-[250px] overflow-hidden items-center">
                 <img className="col-start-1 col-end-4 row-start-1 row-end-4 object-cover aspect-square" src={src}>
                 </img>
                 <div className="col-start-2 row-start-2 col-end-3 row-end-3 flex flex-col justify-around items-center">
-                    <Heading variant="h2" color="text-white" >{title}</Heading>
                     <p className="text-white font-bold">{gender}</p>
+                    <Heading variant="h3" color="text-white" >{title}</Heading>
                 </div>
             </div>
         </Link>
--- pandora/src/layouts/PageContents/ShopContent.jsx
+++ pandora/src/layouts/PageContents/ShopContent.jsx
@@ -2,8 +2,20 @@ import SearchResults from "@/components/SearchResults";
 import ShopProductCards from "../ShopProductCards";
 import Categories from "../Categories";
 import Clients from "../Clients";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { fetchProductsByUserChoices } from "@/redux/actions/productActions";
 
 const ShopContent = () => {
+    const { categoryId } = useParams();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        if (categoryId) {
+            dispatch(fetchProductsByUserChoices(categoryId))
+        }
+    }, [categoryId])
 
     return (
         <div className="content flex flex-col gap-20">
