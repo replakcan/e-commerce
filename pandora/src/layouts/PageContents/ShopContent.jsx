192887e305c3abecd54e3f601f23be7f6b82f879
Alper <alper.makcan@gmail.com>
2024-11-08 13:57:37 +0300

    Fetches products after params are loaded to reducer
    otherwise fetches all products with /shop URL

--- pandora/src/layouts/PageContents/ShopContent.jsx
+++ pandora/src/layouts/PageContents/ShopContent.jsx
@@ -2,12 +2,13 @@ import SearchResults from "@/components/SearchResults";
 import ShopProductCards from "../ShopProductCards";
 import Categories from "../Categories";
 import Clients from "../Clients";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useHistory, useParams, useLocation } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchProductsByUserChoices, setSort, setFilter, setLimit, setOffset, fetchProducts } from "@/redux/actions/productActions";
 
 const ShopContent = () => {
+    const [isParamsLoaded, setIsParamsLoaded] = useState(false);
     const dispatch = useDispatch();
     const history = useHistory();
     const location = useLocation();
@@ -19,6 +20,8 @@ const ShopContent = () => {
     const filter = useSelector((store) => store.product.filter);
     const limit = useSelector((store) => store.product.limit);
     const offset = useSelector((store) => store.product.offset);
+
+    // Yükleme tamamlandıktan sonra ürünleri çekmek için bir flag kullanıyoruz
 
     // Sayfa yüklendiğinde URL'den Redux store'a parametreleri yükle
     useEffect(() => {
@@ -33,32 +36,34 @@ const ShopContent = () => {
         dispatch(setFilter(initialFilter));
         dispatch(setLimit(initialLimit));
         dispatch(setOffset(initialOffset));
+
+        // Parametreler yüklendikten sonra flag'i true yapıyoruz
+        setIsParamsLoaded(true);
     }, [location.search, dispatch]);
 
     useEffect(() => {
-        // Boş olmayan parametreleri dinamik olarak queryParams nesnesine ekle
+        if (isParamsLoaded) {
+            // Boş olmayan parametreleri dinamik olarak queryParams nesnesine ekle
+            const queryParamsObject = {};
+            if (sort) queryParamsObject.sort = sort;
+            if (filter) queryParamsObject.filter = filter;
+            if (limit) queryParamsObject.limit = limit;
+            if (offset) queryParamsObject.offset = offset;
 
+            const queryParams = new URLSearchParams(queryParamsObject);
 
-        const queryParamsObject = {};
-        if (sort) queryParamsObject.sort = sort;
-        if (filter) queryParamsObject.filter = filter;
-        if (limit) queryParamsObject.limit = limit;
-        if (offset) queryParamsObject.offset = offset;
-
-        const queryParams = new URLSearchParams(queryParamsObject);
-
-        if (!(gender && categoryId && categoryName)) {
-            history.push("/shop")
-            dispatch(fetchProducts())
-        }
-
-        // Ürünleri filtreler
-        if (categoryId) {
-            dispatch(fetchProductsByUserChoices(categoryId, sort, filter, limit, offset));
-
+            // Yeni URL'yi güncelle
             history.push(`/shop/${gender}/${categoryName}/${categoryId}?${queryParams.toString()}`);
+
+            // Ürünleri filtreler
+            if (categoryId) {
+                dispatch(fetchProductsByUserChoices(categoryId, sort, filter, limit, offset));
+            } else {
+                dispatch(fetchProducts())
+                history.push("/shop")
+            }
         }
-    }, [gender, categoryId, categoryName, sort, filter, limit, offset, dispatch, history]);
+    }, [isParamsLoaded, gender, categoryId, categoryName, sort, filter, limit, offset, dispatch, history]);
 
     return (
         <div className="content flex flex-col gap-20">
