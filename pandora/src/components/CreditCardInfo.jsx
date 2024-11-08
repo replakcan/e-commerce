73820bcb94bf0b79a3d828f2178929822a1bf12e
Alper <alper.makcan@gmail.com>
2024-11-08 19:41:26 +0300

    Updates create Order page address and payment cards

--- pandora/src/components/CreditCardInfo.jsx
+++ pandora/src/components/CreditCardInfo.jsx
@@ -1,5 +1,4 @@
 import { useDispatch, useSelector } from "react-redux";
-import Heading from "./ui/heading";
 import { Trash } from "lucide-react";
 import { deleteCreditCard, fetchCreditCards } from "@/redux/actions/clientActions";
 import { useState } from "react";
@@ -25,7 +24,7 @@ const CreditCardInfo = ({ id, card_no, expire_month, expire_year, name_on_card, 
     };
 
     return (
-        <div className="border border-black flex justify-between p-2 w-full">
+        <div className="flex border justify-between p-2 w-full cursor-pointer p-4 hover:bg-gray-100 hover:shadow-md transition-all duration-200 rounded-lg">
             {isEditing ? (
                 <CreditCardUpdateForm
                     id={id}
@@ -39,7 +38,7 @@ const CreditCardInfo = ({ id, card_no, expire_month, expire_year, name_on_card, 
                     <p><span className="text-danger font-bold">Son Kullanma Tarihi:</span> {expire_month}/{expire_year}</p>
                 </div>
             )}
-            <div className="flex flex-col justify-between items-end">
+            <div className="flex flex-col gap-4 justify-between items-end">
                 {!isEditing && (
                     <Button onClick={handleEditClick} variant="outline">Kartı Düzenle</Button>
                 )}
