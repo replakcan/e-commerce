b0fb0987d82dde0b3161f6a044c26dc51925dc44
Alper <alper.makcan@gmail.com>
2024-11-09 19:23:57 +0300

    Changes text shown on hover

--- pandora/src/layouts/ProductCard_2.jsx
+++ pandora/src/layouts/ProductCard_2.jsx
@@ -40,7 +40,7 @@ const ProductCard_2 = ({ mostPopi_2 }) => {
                         alt={`${mostPopi_2?.name}`}
                     />
                     <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                        <p className="text-brilliant text-lg font-bold">Go to Product Details</p>
+                        <p className="text-brilliant text-lg font-bold">click to see <br/>product details</p>
                     </div>
                 </div>
 
--- pandora/src/layouts/ProductCard_4.jsx
+++ pandora/src/layouts/ProductCard_4.jsx
@@ -43,7 +43,7 @@ const ProductCard_4 = ({ mostPopi }) => {
                         />
                         {/* Overlay Text */}
                         <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                            <p className="text-brilliant text-lg font-bold">Go to Product Details</p>
+                            <p className="text-brilliant text-lg font-bold">click to see <br />product details</p>
                         </div>
                     </div>
 
