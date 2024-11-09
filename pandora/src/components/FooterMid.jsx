0ff3ce2b16e986c4f1c5fe25dec8e914dd2fa80d
Alper <alper.makcan@gmail.com>
2024-11-09 19:14:06 +0300

    Removes footerTop
    because it was unneccessarily too big!

--- pandora/src/components/FooterMid.jsx
+++ pandora/src/components/FooterMid.jsx
@@ -4,7 +4,7 @@ const FooterMid = () => {
 
     return (
 
-        <div className="footer-el footerMid px-[10%] py-12 bg-white">
+        <div className="footer-el footerMid px-[10%] py-12 text-white bg-gray-800">
             <FooterInfoContent />
         </div>
     )
