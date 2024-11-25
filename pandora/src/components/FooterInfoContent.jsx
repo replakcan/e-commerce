ee2a3ac276acf66f276fed6f126d0e4234a9cc21
replakcan <alper.makcan@gmail.com>
2024-11-25 12:40:10 +0300

    Rearranges footer flex col/row responsive

--- pandora/src/components/FooterInfoContent.jsx
+++ pandora/src/components/FooterInfoContent.jsx
@@ -6,7 +6,7 @@ const FooterInfoContent = () => {
 
     return (
 
-        <div className="footer-info-content flex flex-col lg:flex-row justify-start items-start gap-12">
+        <div className="footer-info-content flex flex-col md:flex-row justify-start items-start gap-12">
             {footerMidContent.map((content, index) => {
                 return <FooterInfoCard
                     key={index}
--- pandora/src/layouts/Footer.jsx
+++ pandora/src/layouts/Footer.jsx
@@ -1,13 +1,11 @@
 import FooterBottom from "@/components/FooterBottom";
 import FooterMid from "@/components/FooterMid";
-import FooterTop from "@/components/FooterTop";
 
 const Footer = () => {
 
 
     return (
         <div className="layout flex flex-col gap-8 bg-gray-50 text-black">
-            {/* <FooterTop /> */}
             <FooterMid />
             <FooterBottom />
         </div>
