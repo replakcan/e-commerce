6329d95de3290cbcc40e36cd180d502667e3174e
replakcan <alper.makcan@gmail.com>
2024-11-25 12:40:30 +0300

    Hides confirm-order btn conditionally

--- pandora/src/components/OrderSummary.jsx
+++ pandora/src/components/OrderSummary.jsx
@@ -1,4 +1,4 @@
-import { useHistory } from "react-router-dom";
+import { useHistory, useLocation } from "react-router-dom";
 import { Button } from "./ui/button";
 import { useDispatch, useSelector } from "react-redux";
 import { ChevronRight } from "lucide-react";
@@ -8,14 +8,14 @@ const shippingTotal = 7.99;
 let kargoBedeli = 7.99;
 
 const OrderSumm = () => {
-
     const cartItems = useSelector((store) => store.shoppingCart.cart);
     const user = useSelector((store) => store.client.user);
-    let history = useHistory();
+    const history = useHistory();
+    const location = useLocation(); // Mevcut URL'i kontrol etmek için kullanılıyor.
     const dispatch = useDispatch();
-    const cartAddress = useSelector((store) => store.shoppingCart.address)
-    const cartPayment = useSelector((store) => store.shoppingCart.payment)
-    const cart = useSelector((store) => store.shoppingCart)
+    const cartAddress = useSelector((store) => store.shoppingCart.address);
+    const cartPayment = useSelector((store) => store.shoppingCart.payment);
+    const cart = useSelector((store) => store.shoppingCart);
 
     const orderTotal = cartItems
         .filter(item => item.checked)
@@ -26,7 +26,7 @@ const OrderSumm = () => {
         kargoBedeli = 0;
     }
 
-    let siparisToplam = orderTotal + kargoBedeli;
+    let siparisToplam = parseFloat(orderTotal) + kargoBedeli;
 
     const orderData = {
         address_id: cart.address.id,
@@ -36,7 +36,7 @@ const OrderSumm = () => {
         card_expire_month: cart.payment.expire_month,
         card_expire_year: cart.payment.expire_year,
         card_ccv: cart.payment.card_ccv,
-        price: siparisToplam, // örnek price 
+        price: siparisToplam, // örnek fiyat
         products: cart.cart.map(item => ({
             product_id: item.product.id,
             count: item.count,
@@ -53,14 +53,14 @@ const OrderSumm = () => {
     };
 
     const sendOrder = () => {
-        console.log("orderData:", orderData)
-        dispatch(createOrder(orderData, user.token))
-    }
+        console.log("orderData:", orderData);
+        dispatch(createOrder(orderData, user.token));
+    };
 
+    const isPreviousOrdersPage = location.pathname === "/confirm-order";
 
     return (
         <div className="order-box h-fit border border-grey-200 p-2 flex flex-col gap-1 md:min-w-[240px]">
-
             <h1 className="font-bold pb-2 border-b border-red-300">SİPARİŞ ÖZETİ</h1>
             <div className="flex justify-between items-center">
                 <h2>Product TOTAL:</h2> <span>{orderTotal}$</span>
@@ -68,30 +68,40 @@ const OrderSumm = () => {
             <div className="flex justify-between items-center">
                 <h2>Shipping total:</h2> <span>{shippingTotal}$</span>
             </div>
-            {
-                orderTotal >= 200 &&
+            {orderTotal >= 200 && (
                 <div className="flex justify-between items-center">
                     <h2 className="italic text-ikincil max-w-[50%]">200 dolar ve üzeri siparislerde kargo bedava</h2>
                     <span className="italic">
-                        {shippingTotal * (-1)}$
+                        {shippingTotal * -1}$
                     </span>
                 </div>
-            }
+            )}
             <div className="flex justify-between items-center">
                 <h1 className="text-2xl">ORDER TOTAL:</h1>
                 <span>{siparisToplam}$</span>
             </div>
-            <Button onClick={handleConfirmOrder} variant="destructive">Sepeti Onayla <ChevronRight /></Button>
-            {cartAddress.title && <div>
-                <p>Teslimat Adresi: <span className="font-bold text-ikincil">{cartAddress.title}</span></p>
-            </div>}
-            {cartPayment.name_on_card && <div>
-                <p>Ödeme Yöntemi: <span className="font-bold text-ikincil">{cartPayment.name_on_card}</span></p>
-            </div>}
-            {cartAddress.title && cartPayment.name_on_card && <div>
-                <Button onClick={sendOrder} >Siparişi Tamamla</Button>
-            </div>}
+            {!isPreviousOrdersPage && (
+                <Button onClick={handleConfirmOrder} variant="destructive">
+                    Sepeti Onayla <ChevronRight />
+                </Button>
+            )}
+            {cartAddress.title && (
+                <div>
+                    <p>Teslimat Adresi: <span className="font-bold text-ikincil">{cartAddress.title}</span></p>
+                </div>
+            )}
+            {cartPayment.name_on_card && (
+                <div>
+                    <p>Ödeme Yöntemi: <span className="font-bold text-ikincil">{cartPayment.name_on_card}</span></p>
+                </div>
+            )}
+            {cartAddress.title && cartPayment.name_on_card && (
+                <div>
+                    <Button onClick={sendOrder}>Siparişi Tamamla</Button>
+                </div>
+            )}
         </div>
-    )
-}
+    );
+};
+
 export default OrderSumm;
